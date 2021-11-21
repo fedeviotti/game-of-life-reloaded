23b1400ba808401005df541998756e580e6c1464
fedeviotti <fedeviotti@gmail.com>
2021-11-21 13:15:08 +0100

    Feat: add some css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import '../styles/layout.css';
 
 const Layout: React.FC = () => {
   return (
     <>
-      <header>
+      <header className="header">
         <Link to="/">Home</Link>
         <nav>
           <Link to="/game-of-life">Game of Life</Link>
         </nav>
       </header>
-      <main>
+      <main className="content">
         <Outlet />
       </main>
     </>
