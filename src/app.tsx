50407c95cd5512f2b72d22529bf6824f8e5e3752
fedeviotti <fedeviotti@gmail.com>
2021-11-21 10:56:27 +0100

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import GameOfLife from './pages/game-of-life';
+
+const App: React.FC = () => {
+  return <GameOfLife />;
+};
+
+export default App;
---
+++ src/pages/game-of-life.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const GameOfLife: React.FC = () => {
+  return <h1>Game of Life</h1>;
+};
+
+export default GameOfLife;
