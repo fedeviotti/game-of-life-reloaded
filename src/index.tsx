81809f7eee2e3d6880fab832044c11033e744b51
fedeviotti <fedeviotti@gmail.com>
2021-11-27 15:01:55 +0100

    Feat: add Sentry monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './app';
 import { store } from './store/store';
+
+Sentry.init({
+  dsn: 'https://ce88c11ee28e4c32b5bea9629f3ad2f2@o1079106.ingest.sentry.io/6083740',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
